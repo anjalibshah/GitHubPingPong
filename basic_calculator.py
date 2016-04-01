ad0b44e394f9f1dce0f1ce2628271224e3396ceb
Roni Kobrosly <roni.kobrosly@gmail.com>
2016-04-01 11:31:42 -0400

    back to you!

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,11 +1,6 @@
 class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
-        """
+    
+"""
         Remove all the whitespaces from the expression string.
         
         Build an expression stack. Push operands into the stack until you encounter an operator.
@@ -16,20 +11,40 @@ class Solution(object):
         
         1. Parentheses: When '(' is encountered, push all expression operators and operands onto the stack
         until the ')' is ecountered. This forms a mini-expression for which the steps outlined above should be 
-        repeated.
+        repeated.  
+"""
+
+    # Remove whitespace characters from string 
+    s = "".join(s.split())
+
+    # Initialize expression stack
+    expression = list(s)
+
+    def calculate(self, expression):
         
-        """
-        
-        # Remove whitespace characters from string
-        s.replace(" ","")
-        
-        # Initialize expression stack
-        exprstack = []
-        
-        # Read the string from leftmost character to the rightmost
-        for ch in range(len(s)):
+        # Find complete parenthesis and then recursively call "calculate function" until you
+        # only have a complete math expression to evaluate
+
+        paren_start = 0
+        paren_end = 0
+        index = -1
+        for ch in range(len(expression)):
+            index += 1
             if ch == '(':
+                paren_start = index
                 continue
-            else if isinstance(ch, int):
-                exprstack.append(ch)
-            else if 
+            if ch == ')':
+                paren_end = index
+
+        # Isolate characters contained with parenthesis
+        sub_list = expression[(paren_start + 1):paren_end]
+
+        # This is where the recursion would happen. 
+        calculate(sub_list)
+
+
+
+
+
+
+
